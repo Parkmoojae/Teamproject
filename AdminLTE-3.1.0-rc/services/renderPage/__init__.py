d43f2afc0efcc31e4a9251eeaf5f6b792d077873
정아인(SM시스템) <lovepizza132@kfems.co.kr>
2021-01-26 14:47:23 +0900

    ain

--- AdminLTE-3.1.0-rc/services/renderPage/__init__.py
+++ AdminLTE-3.1.0-rc/services/renderPage/__init__.py
@@ -1,10 +1,16 @@
 from services import app
 from flask import request, jsonify, render_template, session
 
+# @app.route('/')
+# def indexPage():
+#     return render_template('index2.html')
+
 @app.route('/')
 def indexPage():
-    return render_template('index2.html')
+    return render_template('pages/examples/login.html')
 
-@app.route('/pages/tables/data.html')
+
+
+@app.route('/pages/tables/data.html', methods=['POST'])
 def startPage():
     return render_template('pages/tables/data.html')
