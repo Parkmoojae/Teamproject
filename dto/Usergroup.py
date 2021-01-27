9fec7d3e5d25cb3d0b3a7d1d066f46b2bf19ac0d
유태완(티스마트) <mh5421@kfems.co.kr>
2021-01-27 10:43:11 +0900

    12

---
+++ dto/Usergroup.py
@@ -0,0 +1,26 @@
+from sqlalchemy import Column, Integer, VARCHAR, NUMERIC, CHAR, DATETIME, TEXT, BOOLEAN, NUMERIC
+from dataSource import Base
+
+__all__ = ['Usergroup']
+
+
+class Usergroup(Base):
+    __tableName__ = "usergroup"
+    __table_args__= {
+                'comment' : '회원등급'
+    }
+
+    USER_GROUP_ID                   = Column(VARCHAR(100),primary_key = True, comment="회원등급 id")
+    USER_GROUP_NAME                 = Column(VARCHAR(100), comment="회원등급 이름")
+    
+
+    def __init__(self, ugid, **kwargs):
+        self.USER_GROUP_ID = ugid
+        self.USER_GROUP_NAME                = kwargs.get('USER_GROUP_ID',None)
+
+    def __def__(self):
+        return "{ 'USER_GROUP_ID' : '%s',\
+            'USER_GROUP_NAME' : '%s'}" %{
+                self.USER_GROUP_ID,
+                self.USER_GROUP_NAME
+            }
