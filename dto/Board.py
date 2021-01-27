96752d3844966681f1e35a89986463af54728f10
정아인(SM시스템) <lovepizza132@kfems.co.kr>
2021-01-27 10:17:28 +0900

    dto bard 추가

--- dto/Board.py
+++ dto/Board.py
@@ -1,51 +1,47 @@
 from sqlalchemy import Column, Integer, VARCHAR, NUMERIC, CHAR, DATETIME, TEXT, BOOLEAN, NUMERIC
 from dataSource import Base
 
-__all__ = ['Auth']
+__all__ = ['Board']
 
-class Auth(Base):
-    __tablename__ = "auth"
+class Board(Base):
+    __tablename__ = "Board"
     __table_args__ = {
-                        'comment' : '게시판 권한'
+                        'comment' : '게시판 공통'
     }
 
-    AUTH_ID                              = Column(VARCHAR(100), primary_key=True, comment='게시판 권한 id')
-    USER_GROUP_ID                        = Column(VARCHAR(100), nullable=False, comment='회원그룹 id')
-    BOARD_ID                             = Column(Integer, nullable=False, comment='게시판 id')
-    AUTH_BOARD_READ                      = Column(BOOLEAN, comment='게시판 조회 권한')
-    AUTH_BOARD_WRITE                     = Column(BOOLEAN, comment='게시판 작성 권한')
-    AUTH_BOARD_MODI                      = Column(BOOLEAN, comment='게시판 수정 권한')
-    AUTH_BOARD_DEL                       = Column(BOOLEAN, comment='게시판 삭제 권한')
-    AUTH_COMMENT_DEL                     = Column(BOOLEAN, comment='게시판 댓글 삭제 권한')
-    AUTH_COMMENT_WRITE                   = Column(BOOLEAN, comment='게시판 댓글 작성 권한')
+    BOARD_ID                             = Column(Integer, primary_key=True, autoincrement=True, comment='게시글 id')
+    BOARD_CONTENT                        = Column(VARCHAR(255), nullable=False, comment='게시글 내용')
+    USER_ID                              = Column(VARCHAR(100), nullable=False, comment='회원 id')
+    BOARD_REGDATE                        = Column(DATETIME, comment='게시글 등록일')
+    BOARD_DEL                            = Column(BOOLEAN, comment='게시글 삭제여부 / 일반:0 삭제:1')
+    BOARD_LIST_ID                        = Column(VARCHAR(100), comment='게시판 리스트 id')
+    BOARD_TITLE                          = Column(VARCHAR(100), comment='게시글 제목')
+    BOARD_PID                            = Column(Integer, comment='게시글 pid')
     
-    def __init__(self, authId, **kwargs):
-        self.AUTH_ID = authId
-        self.USER_GROUP_ID                       = kwargs.get('USER_GROUP_ID', None)
-        self.BOARD_ID                            = kwargs.get('BOARD_ID', None)
-        self.AUTH_BOARD_READ                     = kwargs.get('AUTH_BOARD_READ', None)
-        self.AUTH_BOARD_WRITE                    = kwargs.get('AUTH_BOARD_WRITE', None)
-        self.AUTH_BOARD_MODI                     = kwargs.get('AUTH_BOARD_MODI', None)
-        self.AUTH_BOARD_DEL                      = kwargs.get('AUTH_BOARD_DEL', None)
-        self.AUTH_COMMENT_DEL                    = kwargs.get('AUTH_COMMENT_DEL', None)
-        self.AUTH_COMMENT_WRITE                  = kwargs.get('AUTH_COMMENT_WRITE', None)
+    def __init__(self, boardId, **kwargs):
+        self.BOARD_ID = boardId
+        self.BOARD_CONTENT                       = kwargs.get('BOARD_CONTENT', None)
+        self.USER_ID                             = kwargs.get('USER_ID', None)
+        self.BOARD_REGDATE                       = kwargs.get('BOARD_REGDATE', None)
+        self.BOARD_DEL                           = kwargs.get('BOARD_DEL', None)
+        self.BOARD_LIST_ID                       = kwargs.get('BOARD_LIST_ID', None)
+        self.BOARD_TITLE                         = kwargs.get('BOARD_TITLE', None)
+        self.BOARD_PID                           = kwargs.get('BOARD_PID', None)
 
     def __repr__(self):
-        return "{'AUTH_ID' : '%s', \
-        'USER_GROUP_ID' : '%s', \
-        'BOARD_ID' : '%s', \
-        'AUTH_BOARD_READ' : '%s', \
-        'AUTH_BOARD_WRITE' : '%s', \
-        'AUTH_BOARD_MODI' : '%s', \
-        'AUTH_BOARD_DEL' : '%s', \
-        'AUTH_COMMENT_DEL' : '%s', \
-        'AUTH_COMMENT_WRITE' : '%s'}" % (
-                    self.AUTH_ID,
-                    self.USER_GROUP_ID,
+        return "{'BOARD_ID' : '%s', \
+        'BOARD_CONTENT' : '%s', \
+        'USER_ID' : '%s', \
+        'BOARD_REGDATE' : '%s', \
+        'BOARD_DEL' : '%s', \
+        'BOARD_LIST_ID' : '%s', \
+        'BOARD_TITLE' : '%s', \
+        'BOARD_PID' : '%s'}" % (
                     self.BOARD_ID,
-                    self.AUTH_BOARD_READ,
-                    self.AUTH_BOARD_WRITE,
-                    self.AUTH_BOARD_MODI,
-                    self.AUTH_BOARD_DEL,
-                    self.AUTH_COMMENT_DEL,
-                    self.AUTH_COMMENT_WRITE)
+                    self.BOARD_CONTENT,
+                    self.USER_ID,
+                    self.BOARD_REGDATE,
+                    self.BOARD_DEL,
+                    self.BOARD_LIST_ID,
+                    self.BOARD_TITLE,
+                    self.BOARD_TITLE)
