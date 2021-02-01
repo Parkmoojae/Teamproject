2ece7353c4fdb2f55e5aafc2a06a5543373028f9
kys123 <kys123@kfems.co.kr>
2021-02-01 14:56:19 +0900

    210201_07

--- services/auth/service.py
+++ services/auth/service.py
@@ -5,18 +5,22 @@ from util import *
 
 def authority_groupAuthority_getList(data):
     result = {}
-    result = session.query(model.Auth)\
+    rows = session.query(model.Auth)\
             .join(model.User, model.Auth.user_group_id == model.User.user_group_id)\
             .with_entities(
                 model.User.user_id,
                 model.User.user_group_id,
                 model.Auth.auth_id,
-                model.Auth.board_id,
+                model.Auth.board_list_id,
                 model.Auth.auth_board_write,
                 model.Auth.auth_board_del,
                 model.Auth.auth_board_modi,
                 model.Auth.auth_board_read,
                 model.Auth.auth_comment_write,
                 model.Auth.auth_comment_del
-            ).filter(model.User.user_id == data['user_id'])
-    return convertStatementToList(result)
+            ).filter(model.User.user_id == data['user_id']).all()
+    
+    for row in rows:
+        result[row.]
+
+    
