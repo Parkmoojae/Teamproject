de55afee738be258eafca2e1777cd98b503407c6
정아인(SM시스템) <lovepizza132@kfems.co.kr>
2021-02-01 15:04:32 +0900

    02011504

--- util/decorator2.py
+++ util/decorator2.py
@@ -7,11 +7,21 @@ def jai_authDecorator(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
         print("decorator!!!!")
+        requestPath = request.path
+        print(requestPath)
         data = {}
         data = request.get_json()
         print(data)
         if(flaskSession.get('loginUserData')):
             data['userId'] = flaskSession.get('loginUserData')['user_id']
+        
+        # 댓글 입력 권한 비교
+        # if(requestPath=='/insertComment'):
+
+        # 댓글 삭제 권한 비교
+        # if(requestPath=='/delComment'):
+            
+
         return f(*args, **kwargs)
 
     return decorated_function
